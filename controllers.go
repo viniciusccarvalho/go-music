39cd73e17b5580e5f82e2b6a2c47e28a5dec0150
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2014-07-22 23:12:18 -0400

    Working just like the java version, only on mongodb

--- controllers.go
+++ controllers.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"net/http"
 	"encoding/json"
-	"io/ioutil"
-	)
+	"fmt"
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"net/http"
+)
 
 type Album struct {
-	Id          string `json:"id"`
-	Title       string `json:"title"`
-	Artist      string `json:"artist"`
-	ReleaseYear string `json:"releaseYear"`
-	Genre       string `json:"genre"`
-	TrackCount  int32  `json:"trackCount"`
-	AlbumId     string `json:"albumId"`
+	Id          bson.ObjectId `json:"id"           bson:"_id"`
+	Title       string        `json:"title"`
+	Artist      string        `json:"artist"`
+	ReleaseYear string        `json:"releaseYear"`
+	Genre       string        `json:"genre"`
+	TrackCount  int32         `json:"trackCount"`
+	AlbumId     string        `json:"albumId"`
 }
 
 type ApplicationInfo struct {
@@ -21,17 +24,83 @@ type ApplicationInfo struct {
 	Services []string `json:"services"`
 }
 
-
 func listAlbums(response http.ResponseWriter, request *http.Request) {
-	js, err := ioutil.ReadFile("albums.json")
+	session, err := openSession()
 	if err != nil {
-		
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	response.Write(js)	
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("").C("album")
+	results := make([]Album, 10)
+	c.Find(bson.M{}).All(&results)
+	js, err := json.Marshal(results)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response.Write(js)
 }
 
+func addAlbum(response http.ResponseWriter, request *http.Request) {
+	decoder := json.NewDecoder(request.Body)
+	album := Album{}
+	err := decoder.Decode(&album)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	
+	if !album.Id.Valid() {
+		album.Id = bson.NewObjectId()
+	}
+	session, err := openSession()
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("").C("album")
+	c.UpsertId(album.Id,&album)
+	response.Write([]byte("{}"))
+}
+
+func deleteAlbum(response http.ResponseWriter, request *http.Request) {
+	id := mux.Vars(request)["id"]
+	session, err := openSession()
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("").C("album")
+	bid := bson.ObjectIdHex(id)
+	c.RemoveId(bid)
+}
+
+
 func info(response http.ResponseWriter, request *http.Request) {
-	appInfo := ApplicationInfo{[]string{"cloud"},[]string{""}}
+	serviceList := []string{}
+
+	for service, _ := range env.services {
+		serviceList = append(serviceList, service)
+	}
+
+	appInfo := ApplicationInfo{[]string{env.profile}, serviceList}
 	js, _ := json.Marshal(appInfo)
 	response.Write(js)
+}
+
+func openSession() (*mgo.Session, error) {
+	service := env.uri("document")
+	session, err := mgo.Dial(fmt.Sprintf("%v", service.Credentials["uri"]))
+
+	return session, err
 }
--- server.go
+++ server.go
@@ -1,34 +1,113 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"os"
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"net/http"
+	"io/ioutil"
+	"os"
 )
 
+var env = NewEnvironment()
+
+func init() {
+	session, err := openSession()
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("").C("album")
+	
+	total, _ := c.Count()
+	fmt.Printf("Found %v documents\n",total)
+	if total == 0 {
+		fmt.Println("Starting DB ...")
+		data, err := ioutil.ReadFile("albums.json")
+		if err != nil {
+			fmt.Print("Could not open albums.json")
+		}
+		albums := make([]Album,10)
+		err = json.Unmarshal([]byte(data),&albums)
+		for _, album := range albums {
+			album.Id = bson.NewObjectId()
+			err = c.Insert(&album)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		
+	}
+}
+
 func main() {
-	fmt.Println("Starting go server ...")
 	router := mux.NewRouter()
-	
-	router.HandleFunc("/echo",echo)
-	router.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) {
-				http.ServeFile(w,r,"./static/index.html")
-			}).Methods("GET")
-	router.HandleFunc("/albums",listAlbums).Methods("GET")
-	router.HandleFunc("/info",info).Methods("GET")
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/index.html")
+	}).Methods("GET")
+	router.HandleFunc("/albums", listAlbums).Methods("GET")
+	router.HandleFunc("/albums",addAlbum).Methods("POST","PUT")
+	router.HandleFunc("/info", info).Methods("GET")
+	router.HandleFunc("/albums/{id}",deleteAlbum).Methods("DELETE")
+
 	port := os.Getenv("PORT")
-	fmt.Println("Using port ", port) 
-	
+	if len(port) == 0 {
+		port = "9000"
+	}
+
+	fmt.Println("Starting Go server on port ", port)
+
 	http.Handle("/", router)
-	http.Handle("/static/",http.FileServer(http.Dir("./")))
+	http.Handle("/static/", http.FileServer(http.Dir("./")))
 	http.ListenAndServe(":"+port, nil)
 }
 
-func echo(response http.ResponseWriter, request *http.Request) {
+//--------------------------- Support types --------------------------------------//
 
-	fmt.Fprint(response, "{\"message\": \"Hello World!\"}")
+type Environment struct {
+	services map[string][]ServiceDefinition
+	profile  string
 }
 
+type ServiceDefinition struct {
+	Name        string                 `json:"name"`
+	Label       string                 `json:"label"`
+	Tags        []string               `json:"tags"`
+	Plan        string                 `json:"plan"`
+	Credentials map[string]interface{} `json:"credentials"`
+}
 
+func NewEnvironment() *Environment {
+	env := Environment{}
+	err := json.Unmarshal([]byte(os.Getenv("VCAP_SERVICES")), &env.services)
+	if err != nil {
+		fmt.Errorf("error decoding string", err)
+	}
+	vcap := os.Getenv("VCAP_APPLICATION")
+	if len(vcap) == 0 {
+		env.profile = "local"
+	} else {
+		env.profile = "cloud"
+	}
+	return &env
+}
 
+func (this Environment) uri(serviceTag string) ServiceDefinition {
+	if this.profile == "local" {
+		service := ServiceDefinition{}
+		service.Credentials["uri"] = "localhost"
+		return service
+	}
+	for key, _ := range this.services {
+		for _, service := range this.services[key] {
+			return service
+		}
+	}
+	return ServiceDefinition{}
+}
